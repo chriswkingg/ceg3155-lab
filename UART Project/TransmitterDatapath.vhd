7c5babce8f4d71e35ee24d4561df0262caec8602
Chriswkingg <chriswkingg@gmail.com>
2023-12-03 20:52:59 -0500

    Started Tx datapath WIP

--- UART Project/TransmitterDatapath.vhd
+++ UART Project/TransmitterDatapath.vhd
@@ -5,23 +5,23 @@ ENTITY TransmitterDatapath IS
     PORT (
         i_resetBar : IN STD_LOGIC;
         i_clock : IN STD_LOGIC;
-        i_TDRE : IN STD_LOGIC;
-        o_setTDRE, o_resetTDRE, o_rightShiftTSR, o_SO0, o_SO1, o_loadTDR, o_loadTSR : OUT STD_LOGIC;
-		  o_s : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
+        i_TDR : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		  i_loadTDR : IN STD_LOGIC;
+		  i_rightShiftTSR, i_loadTSR : IN STD_LOGIC;
+        i_SO0, i_SO1 : IN STD_LOGIC
+		  o_tx : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
     );
 END TransmitterDatapath;
 
 ARCHITECTURE structural OF TransmitterDatapath IS
-    COMPONENT dflipflop
-        PORT (
-            i_d : IN STD_LOGIC;
-            i_clock : IN STD_LOGIC;
-            i_enable : IN STD_LOGIC;
-				i_async_reset : IN STD_LOGIC;
-				i_async_set : IN STD_LOGIC;
-            o_q, o_qBar : OUT STD_LOGIC
-        );
-    END COMPONENT;
+COMPONENT eightBitShiftRegisterStructural
+PORT (
+	  i_resetBar, i_load, i_shiftLeft, i_shiftRight : IN STD_LOGIC;
+	  i_clock : IN STD_LOGIC;
+	  i_Value : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+	  o_Value : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+);
+END COMPONENT;
 
 BEGIN
 -- wip
